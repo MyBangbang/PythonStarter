53a462188ca17ffeef46851795d7c75b3c970d0c
MrBangbang <1013189061@qq.com>
2016-07-24 09:53:38 +0800

    Python第十三个程序:参数, 解包, 变量

---
+++ ex13.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding=utf-8
+#参数,解包,变量
+
+from sys import argv
+
+script, first, secong, third = argv
+
+print "The scropt is calles:", script
+print "Your first variable is:", first
+print "Your second variable is:" secong
+print "Your third variable is:", third
+
